ae783245a9c8004a89b0b825f7628bda701e2567
biretbits <lipiri12345678xp@example.com>
2024-05-16 22:09:36 -0400

    16-05-2024 22:09

--- librerias/admin/headeradmin.php
+++ librerias/admin/headeradmin.php
@@ -15,7 +15,7 @@
         <script src="activos/js/sweetAlert2/sweetalert2.min.js"></script>
         <link rel="stylesheet" href="activos/js/sweetAlert2/sweetalert2.min.css">
         <link href="activos/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="imagenes/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
 <style>.CodeMirror {border-top: 1px solid black; border-bottom: 1px solid black;}</style>
 <style type="text/css">
 
@@ -49,12 +49,12 @@ echo "<nav class='navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-s
         <div class='collapse navbar-collapse' id='navbarNav'>
             <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
                 <li class='nav-item' title='Inicio'>
-                  <a class='nav-link btn btn-outline-secondary' href='controlador/logeo.controlador.php?accion=ix'><i class='fas fa-home'></i></a>
+                  <a class='nav-link btn btn-outline-warning' href='controlador/logeo.controlador.php?accion=ix'><img src='imagenes/iconos/casa.png'style='height: 25px;width: 25px;'></a>
                 </li>";
               if(isset($_SESSION["tipo_usuario"]) && $_SESSION["tipo_usuario"] == "admin"){
               echo "
                 <li class='nav-item ms-2' title='Usuarios'>
-                    <a class='nav-link btn btn-outline-warning' href='controlador/usuario.controlador.php?accion=vut'> <i class='fas fa-user'></i></a>
+                    <a class='nav-link btn btn-outline-warning' href='controlador/usuario.controlador.php?accion=vut'><img src='imagenes/iconos/admin.png'style='height: 25px;width: 25px;'></a>
                 </li>";
               }
      echo "</ul>
@@ -71,7 +71,7 @@ echo "<nav class='navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-s
                   </li>
                   <li><hr class='dropdown-divider'></li>
                   <li class='nav-item' title='Cerrar sesión' style='color:green'>
-                    <a class='nav-link text-primary btn btn-outline-danger' href='controlador/logeo.controlador.php?accion=salir'><i class='fas fa-power-off'></i></a>
+                    <a class='nav-link text-primary btn btn-outline-danger' href='controlador/logeo.controlador.php?accion=salir'><img src='imagenes/iconos/apagar.png'style='height: 25px;width: 25px;'></a>
                   </li>
               </ul>
               </li>";
