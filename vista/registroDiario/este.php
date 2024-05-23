1fb27e0d99b1f5b79b70d027a285e0bc7747c8d2
biretbits <lipiri12345678xp@example.com>
2024-05-23 19:20:30 -0400

    prueba

---
+++ vista/registroDiario/este.php
@@ -0,0 +1,19 @@
+<?php
+include("require/ama/456.php");
+
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <input type="text" name="" value="nombre">
+     <input type="fdgf" name="" value="niaa">
+     <input type="text" name="" value="mama">
+   </body>
+ </html>
