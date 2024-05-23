e63b90bc0c00dacb608010bd8de9e75a8263d57a
biretbits <lipiri12345678xp@example.com>
2024-05-23 19:31:02 -0400

    44

--- vista/registroDiario/este.php
+++ vista/registroDiario/este.php
@@ -4,7 +4,21 @@ include("require/ama/456.php");
 
 
  ?>
-
+<script type="text/javascript">
+  alert(456465);
+</script>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+      alert(45646)
+    </script>
+  </body>
+</html>
  <!DOCTYPE html>
  <html lang="en" dir="ltr">
    <head>
@@ -20,5 +34,13 @@ include("require/ama/456.php");
      <input type="number" name="" value="">
      <button type="button" name="button" onclick="aumentar"></button>
      <button type="seguir" name="button"></button>
+     <button type="wilmer" name="button"></button>
    </body>
  </html>
+
+
+ <script type="text/javascript">
+   function uno(){
+     alert(4564213213213);
+   }
+ </script>
